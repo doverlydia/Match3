3dcb92bb79ff28e2d290c3d7c4a0612880d9668a
doverlydia <68915121+doverlydia@users.noreply.github.com>
2021-12-08 01:30:56 +0200

    implemented fill function

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -24,6 +24,7 @@ public class Grid : MonoBehaviour
     void Start()
     {
         PiecePrefabDict = new Dictionary<PieceType, GameObject>();
+
         foreach (var piece in piecePrefabs)
         {
             if (!PiecePrefabDict.ContainsKey(piece.Type))
@@ -31,6 +32,7 @@ public class Grid : MonoBehaviour
                 PiecePrefabDict.Add(piece.Type, piece.prefab);
             }
         }
+
         for (int i = 0; i < xDim; i++)
         {
             for (int j = 0; j < yDim; j++)
@@ -38,7 +40,9 @@ public class Grid : MonoBehaviour
                 GameObject background = Instantiate(backgroundPrefab, GetWorldPosition(i, j), Quaternion.identity, transform);
             }
         }
+
         pieces = new GamePiece[xDim, yDim];
+
         for (int i = 0; i < xDim; i++)
         {
             for (int j = 0; j < yDim; j++)
@@ -47,6 +51,7 @@ public class Grid : MonoBehaviour
             }
         }
 
+        Fill();
     }
     public Vector2 GetWorldPosition(int x, int y)
     {
@@ -57,28 +62,28 @@ public class Grid : MonoBehaviour
     {
         GameObject newPiece = Instantiate(PiecePrefabDict[type], GetWorldPosition(x, y), Quaternion.identity, transform);
         pieces[x, y] = newPiece.GetComponent<GamePiece>();
-        pieces[x,y].Init(x, y, this, type);
+        pieces[x, y].Init(x, y, this, type);
         return pieces[x, y];
     }
 
     public void Fill()
     {
-
+        while (FillStep()) { }
     }
     public bool FillStep()
     {
         bool movedPiece = false;
-        for (int y = yDim-2; y >=0; y--)
+        for (int y = yDim - 2; y >= 0; y--)
         {
             for (int x = 0; x < xDim; x++)
             {
                 GamePiece piece = pieces[x, y];
                 if (piece.IsMovable())
                 {
-                    GamePiece pieceBelow = pieces[x, y+1];
-                    if(pieceBelow.Type == PieceType.Empty)
+                    GamePiece pieceBelow = pieces[x, y + 1];
+                    if (pieceBelow.Type == PieceType.Empty)
                     {
-                        piece.MovableComponent.Move(x, y+1);
+                        piece.MovableComponent.Move(x, y + 1);
                         pieces[x, y + 1] = piece;
                         SpawnNewPiece(x, y, PieceType.Empty);
                         movedPiece = true;
@@ -86,6 +91,23 @@ public class Grid : MonoBehaviour
                 }
             }
         }
+
+        for (int x = 0; x < xDim; x++)
+        {
+            GamePiece pieceBelow = pieces[x, 0];
+            if (pieceBelow.Type == PieceType.Empty)
+            {
+                GameObject newPiece = Instantiate(PiecePrefabDict[PieceType.Normal], GetWorldPosition(x, -1), Quaternion.identity, transform);
+
+                pieces[x, 0] = newPiece.GetComponent<GamePiece>();
+                pieces[x, 0].Init(x, -1, this, PieceType.Normal);
+                pieces[x, 0].MovableComponent.Move(x, 0);
+                pieces[x, 0].ColorComponent.SetColor((ColorType)Random.Range(0, pieces[x, 0].ColorComponent.NumColors));
+                movedPiece = true;
+
+            }
+        }
+
         return movedPiece;
 
     }
